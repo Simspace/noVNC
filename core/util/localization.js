73d0fef544ef268d62a62d745fb02ba9b169baf3
Matthew Russell <matt@simspace.com>
2017-03-08 20:15:56 -0800

    Use var instead of let since its ES5

--- core/util/localization.js
+++ core/util/localization.js
@@ -153,7 +153,7 @@ Localizer.prototype = {
             }
 
             for (var i = 0;i < elem.childNodes.length;i++) {
-                let node = elem.childNodes[i];
+                var node = elem.childNodes[i];
                 if (node.nodeType === node.ELEMENT_NODE) {
                     process(node, enabled);
                 } else if (node.nodeType === node.TEXT_NODE && enabled) {
